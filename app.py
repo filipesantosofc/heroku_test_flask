eb64b18049a3ab7d3abad2eb43e4520bb7306af2
filipesantosofc <156866554+filipesantosofc@users.noreply.github.com>
2024-01-24 00:54:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
+import os
+import requests
 from flask import Flask, request, jsonify
 from gradio_client import Client
-import requests
-import os
 
 app = Flask(__name__)
 
-# Função para salvar a imagem no serviço de armazenamento
-def save_image_to_api(image_path):
-    api_url = "https://wosocial.bubbleapps.io/version-test/api/1.1/wf/save"
-    files = {'file': open(image_path, 'rb')}
-    response = requests.get(api_url, params={'file': image_path}, files=files)
-    return response.json().get('url', '')
+# Diretório temporário para armazenar as imagens geradas
+TEMP_DIR = "/tmp/gradio"
 
 @app.route('/run', methods=['GET'])
 def run_model():
     # ... (código anterior para obter parâmetros)
 
-    # Chamar a API Gradio e salvar a imagem localmente
+    # Chamar a API Gradio
     client = Client("https://squaadai-sd-xl.hf.space/--replicas/yl24o/")
     result = client.predict(
         prompt, negative_prompt, prompt_2, negative_prompt_2,
@@ -28,15 +24,27 @@ def run_model():
         api_name="/run"
     )
 
-    # Salvar a imagem localmente
-    image_path = "/tmp/gradio/output_image.png"
-    result['image'].save(image_path)
+    # Salvar a imagem em um diretório temporário
+    image_path = os.path.join(TEMP_DIR, "image.png")
+    result["image"].save(image_path)
 
-    # Chamar a API para salvar a imagem e obter a URL
-    saved_image_url = save_image_to_api(image_path)
+    # Chamar sua API de salvar imagem para obter a URL s3
+    save_image_url = "https://wosocial.bubbleapps.io/version-test/api/1.1/wf/save?file=image.png"
+    files = {'file': open(image_path, 'rb')}
+    response = requests.get(save_image_url, files=files)
 
-    # Retornar a URL da imagem
-    return jsonify({"saved_image_url": saved_image_url})
+    # Obter a URL s3 da resposta
+    s3_url = response.json().get("url", "")
+
+    # Remover o arquivo temporário após a conclusão
+    os.remove(image_path)
+
+    # Retornar a URL s3
+    return jsonify({"s3_url": s3_url})
 
 if __name__ == '__main__':
+    # Criar o diretório temporário se não existir
+    os.makedirs(TEMP_DIR, exist_ok=True)
+
+    # Executar o aplicativo Flask
     app.run(debug=True)
