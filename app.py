bb215dbaee50fb431a14a4ef4855a5d71c22c852
filipesantosofc <156866554+filipesantosofc@users.noreply.github.com>
2024-01-24 00:24:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 27 09:54:17 2021
-
-@author: shubh
-"""
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return ("Hello")
 
 if __name__ == '__main__':
